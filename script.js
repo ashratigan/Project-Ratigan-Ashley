a5a93d77e2706e3a2181e55c2493490a01b35a2b
Ashley <ashley.ratigan@gmail.com>
2021-02-26 22:21:00 -0500

    cleaned up and added comments

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
+// Function to change background color on care page
+// Colors will go through rainbow 
 function changeBodyColor(){
     var toUpdate = document.body;
 
     switch(toUpdate.style.backgroundColor) {
         case "white":
             toUpdate.style.backgroundColor = "rgb(201, 193, 231)";
-            toUpdate.style.color = "black";
           break;
         case "rgb(201, 193, 231)":
             toUpdate.style.backgroundColor = "rgb(189, 213, 239)";
-            toUpdate.style.color = "black";
           break;
         case "rgb(189, 213, 239)":
             toUpdate.style.backgroundColor = "rgb(199, 227, 208)";
-            toUpdate.style.color = "black";
           break;
         case "rgb(199, 227, 208)":
             toUpdate.style.backgroundColor = "rgb(231, 230, 206)";
-            toUpdate.style.color = "black";
           break;
         case "rgb(231, 230, 206)":
             toUpdate.style.backgroundColor = "rgb(242, 216, 204)";
-            toUpdate.style.color = "black";
           break;
         case "rgb(242, 216, 204)":
             toUpdate.style.backgroundColor = "rgb(233, 204, 206)";
-            toUpdate.style.color = "black";
           break;
         case "rgb(233, 204, 206)":
             toUpdate.style.backgroundColor = "rgb(201, 193, 231)";
-            toUpdate.style.color = "black";
           break;
         default:
             toUpdate.style.backgroundColor = "rgb(201, 193, 231)";
-            toUpdate.style.color = "white";
       }
 }
 
