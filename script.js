9d74bcac53fd013e3338d21db56d43665e072a36
Ashley <ashley.ratigan@gmail.com>
2021-02-27 20:26:37 -0500

    updated where form is submitted to

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// code for form on ready page
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+function bindButtons(){
+  document.getElementById('formSubmit').addEventListener('click', function(event){
+
+    const name = document.getElementById("form-name").value
+    const email = document.getElementById("form-email").value
+    const payload = name + " " + email
+
+    if (payload == " ") {
+      event.preventDefault();
+      document.getElementById('form-response').textContent = "Oops, you need to add a name and email!";
+    } else {
+      event.preventDefault();
+      const req = new XMLHttpRequest();
+      req.open("POST", "http://httpbin.org/post");
+      req.setRequestHeader('Content-Type', 'application/json');
+      req.addEventListener('load',function(){
+        if(req.status >= 200 && req.status < 400){
+          const response = JSON.parse(req.responseText);
+          document.getElementById('ready-form-container').textContent = null;
+          document.getElementById('form-response').textContent = "Success! Thanks for submitting your email";
+        } else {
+          console.log("Error in network request: " + req.statusText);
+          document.getElementById('form-response').textContent = "Oops, something went wrong!";
+        }});
+      req.send(JSON.stringify(payload));
+      }
+  })
+}
+
+
+
 // Function to change background color on care page
 // Colors will go through rainbow 
 function changeBodyColor(){
